d6a7dc712b0a43b253cfe3eea94027443120c769
omutwar <omutwar@hotmail.com>
2023-05-27 13:29:00 -0500

    Pushing all local changes to  remote

--- tests/selenium_test1.py
+++ tests/selenium_test1.py
@@ -6,12 +6,17 @@ chrome = webdriver.Chrome(executable_path='bin/chromedriver/chromedriver.exe')
 
 drivers = [gecko, chrome]
 
-for driver in drivers:
-    driver.maximize_window()
-    time.sleep(2)
-    driver.get('https://www.google.com')
-    time.sleep(2)
-    print('Title of the page is: ' + driver.title)
-    print('Printing current website address: ' + driver.current_url)
-    print('Printing current website handle: ' + driver.current_window_handle)
-    driver.close()
+
+def test_google_page():
+    for driver in drivers:
+        driver.maximize_window()
+        time.sleep(2)
+        driver.get('https://www.google.com')
+        time.sleep(2)
+        print('Title of the page is: ' + driver.title)
+        assert driver.title is not None and driver.title != ""
+        print('Printing current website address: ' + driver.current_url)
+        assert driver.current_url is not None
+        print('Printing current website handle: ' + driver.current_window_handle)
+        assert driver.current_window_handle is not None
+        driver.close()
