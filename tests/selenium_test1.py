4b974865ebc0b56aea3a39894733d3f91cd1d2d6
omutwar <omutwar@hotmail.com>
2023-05-27 19:37:44 -0500

    Updated the project

--- tests/selenium_test1.py
+++ tests/selenium_test1.py
@@ -1,5 +1,8 @@
-from selenium import webdriver
 import time
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 gecko = webdriver.Firefox(executable_path='bin/geckodriver/geckodriver.exe')
 chrome = webdriver.Chrome(executable_path='bin/chromedriver/chromedriver.exe')
@@ -7,6 +10,7 @@ chrome = webdriver.Chrome(executable_path='bin/chromedriver/chromedriver.exe')
 drivers = [gecko, chrome]
 
 
+@pytest.mark.GoogleTitle
 def test_google_page():
     for driver in drivers:
         driver.maximize_window()
@@ -20,3 +24,16 @@ def test_google_page():
         print('Printing current website handle: ' + driver.current_window_handle)
         assert driver.current_window_handle is not None
         driver.close()
+
+
+@pytest.mark.GoogleSearch
+def test_google_signin():
+    chrome.get("https://google.com")
+    chrome.maximize_window()
+    time.sleep(3)
+    chrome.find_element(By.XPATH, '//*[@id="APjFqb"]').send_keys('how to program with python')
+    time.sleep(5)
+
+    time.sleep(2)
+    assert chrome.title is not None
+    chrome.quit()
