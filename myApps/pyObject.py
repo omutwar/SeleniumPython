9db926f815ba14388dfbf0870ba8ad5c294156fc
omutwar <omutwar@hotmail.com>
2024-03-01 13:50:55 -0600

    Renamed working directory

--- myApps/pyObject.py
+++
@@ -1,2 +0,0 @@
-# In Python, # is used for comment
-print("Hello Mamatwelijan!")
--- myApps/pythonBook1.py
+++
@@ -1,9 +0,0 @@
-print('Hello, World')   #TODO: Get familiar with the Pycharm by using it!
-print('Hello, World')   #TODO: This is getting really awesome!
-
-def add(a,b,c):
-    return (a+b+c)
-def mean(sum,qty):
-    return sum/qty
-
-print(mean(add(2,3,5),3))
