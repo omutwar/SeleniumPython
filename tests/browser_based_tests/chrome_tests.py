2716f5e4660c67e349b96f378dc99fdf298d112b
omutwar <omutwar@hotmail.com>
2024-03-01 14:19:15 -0600

    Renamed test files for new changes

--- tests/browser_based_tests/chrome_tests.py
+++ tests/browser_based_tests/chrome_tests.py
@@ -4,27 +4,20 @@
 """
 
 import time
-import os
 
 from selenium import webdriver
-from selenium.webdriver.chrome import service
-
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-
-driver_path = os.getcwd() + 'drivers\\operadriver_win64\\chromedriver.exe'
-print(f'** Driver Path: {driver_path}')
-webdriver_service = service.Service(driver_path)
-webdriver_service.start()
+from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.binary_location = "path/to/operabrowser"
 options.add_experimental_option('w3c', True)
-
-driver = webdriver.Remote(webdriver_service.service_url, options=options)
+driver = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()), options=options)
 
 driver.get('https://www.google.com/')
 input_txt = driver.find_element(By.NAME, 'q')
-input_txt.send_keys('operadriver\n')
+input_txt.send_keys('how to initialize selenium in python without binary file, write a demo test example')
 
 time.sleep(5)  # see the result
 driver.quit()
